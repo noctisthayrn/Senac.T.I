e1acd22a9d2dd84f38cfe882338ef2daf0432d4d
Noctis Lucyan Thayrn <116521892+noctisthayrn@users.noreply.github.com>
2023-09-27 12:26:09 -0300

    Update

--- P.I/P.I (Web, site estatico)/menu-lateral.js
+++
@@ -1,9 +0,0 @@
-//Controle para expandir a barra lateral quando clicar no botão de três barras
-
-var btnExp = document.querySelector('#btn-exp')
-
-var menuSide = document.querySelector('.menu-lateral')
-
-btnExp.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
